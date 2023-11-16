092dceaf6c25589d7c855265138f869466274fe8
Damian <damian@example.com>
2023-11-16 15:05:09 +0100

    added student classes

---
+++ src/main/java/com/example/schoolProject/repository/StudentRepository.java
@@ -0,0 +1,16 @@
+package com.example.schoolProject.repository;
+
+import com.example.schoolProject.domain.StudentEntity;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Date;
+import java.util.List;
+
+@Repository
+@Transactional
+public interface StudentRepository extends JpaRepository<StudentEntity, Long> {
+    StudentEntity findByName(String name);
+    List<StudentEntity> findAll();
+}
